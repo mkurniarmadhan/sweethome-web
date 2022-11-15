37d412653770fb398ce911d1a81630c7ee0009b1
junizapermatanugraheni <junizanugraheni@gmail.com>
2022-11-15 22:36:08 +0700

    update header, kontak, beranda, tentang

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,59 @@
-<h1>TEST</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- header -->
+    <?php echo file_get_contents('../templates/header.php') ?>
+
+    <!-- halaman beranda -->
+    <div class="container-fluid py-0 py-md-0 px-5 px-md-4" style="background: linear-gradient(to right, rgba(83,100,186,1), rgba(177,181,205,1))">
+        <div class="row">
+            <div class="col-lg-5 ms-5 align-self-center">
+                <h1 class="text-white fw-bold lh-base" style="font-size: 40px">Temukan Rumah Impianmu</h1>
+                <h5 class="text-white mt-4 fw-bold" style="font-size: 20px">Sweet Home</h5>
+                <ul class="list-unstyled small text-muted">
+                    <li class="mb-2 text-white mt-3 fw-light" style="font-size: 20px">
+                        Hadir untuk membantu kamu dalam menemukan rumah impian yang kamu dambakan dengan harga terjangkau namun berkualitas</li>
+                </ul>
+                <button class="btn text-light" style="background-color: #3649AC; align-s: end;">Temukan <i class="bi bi-chevron-right"></i></button> 
+            </div>
+            <div class="col-lg-5 mt-5 align-self-end">
+                <img src="../../../public/dist/img/img1.png" height="495px" width="674px">
+            </div>
+        </div>
+    </div>
+    <!-- end halaman beranda -->
+    <div class="container-fluid py-5 py-md-0 px-5 px-md-2" style="background: #c7ccea">
+        <div class="row">
+            <div class="col-lg-6 mt-5 ms-5 align-self-center">
+                <div class="image1">
+                    <img src="../../../public/dist/img/image 17.png" style="z-index: 0; position:absolute;">
+                </div>
+                <div class="image2 positio-relative">
+                    <img src="../../../public/dist/img/image 18.png" style="z-index: 1; position:relative; left: 180px; top: 50px; bottom: 100px;">
+                </div>
+            </div>
+            <div class="col-lg-4 ms-5 align-self-center">
+                <h1 class="text-white fw-bold lh-base" style="font-size: 40px">Temukan Rumah Impianmu</h1>
+                <h5 class="text-white mt-4 fw-bold" style="font-size: 20px">Sweet Home</h5>
+                <ul class="list-unstyled small text-muted">
+                    <li class="mb-2 text-white mt-3 fw-light" style="font-size: 20px">
+                        Hadir untuk membantu kamu dalam menemukan rumah impian yang kamu dambakan dengan harga terjangkau namun berkualitas</li>
+                </ul>
+                <button class="btn text-light" style="background-color: #3649AC; align-s: end;">Temukan <i class="bi bi-chevron-right"></i></button> 
+            </div>
+        </div>
+    </div>
+    <!-- halaman tentang -->
+    <!-- end halaman tentang -->
+
+    <!-- footer -->
+    <?php echo file_get_contents('../templates/footer.php') ?>
+    
+</body>
+</html>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,9 +1,90 @@
-
-
-<!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-<!-- Bootstrap 5 -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <!-- Bootstrap 5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- link icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <title>Footer</title>
+</head>
+<body>
+    <footer class="bd-footer py-4 py-md=5 mt-5" style="background-color: #2F3868">
+    <div class="container py-2 py-md-3 px-2 px-md-1">
+        <div class="row">
+            <div class="col-lg-6 mb-3">
+                <h5 class="text-white">Sweet Home</h5>
+                <ul class="list-unstyled small text-muted">
+                    <li class="mb-2 text-white">Hadir untuk membantu kamu dalam menemukan rumah impian yang kamu dambakan dengan harga terjangkau namun berkualitas</li>
+                </ul>
+                <div class="row row-cols-1 row-cols-md-3 g-4" >
+                    <div class="col">
+                        <div class="card" style="background-color: #BDC2DD">
+                            <div class="card-body text-center">
+                                <p class="card-text">
+                                    Berdiri Sejak
+                                </p>
+                                <h6 class="card-title">
+                                    2000
+                                </h6>
+                                
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col">
+                        <div class="card" style="background-color: #BDC2DD">
+                            <div class="card-body text-center">
+                                <p class="card-text">
+                                    Keluarga Percaya
+                                </p>
+                                <h6 class="card-title">
+                                    1000+
+                                </h6>
+                                
+                            </div>
+                        </div> 
+                    </div> 
+                    <div class="col">
+                        <div class="card" style="background-color: #BDC2DD">
+                            <div class="card-body text-center">
+                                <p class="card-text">
+                                    Penghargaan
+                                </p>
+                                <h6 class="card-title">
+                                    5+
+                                </h6>
+                                
+                            </div>
+                        </div> 
+                    </div> 
+                </div>
+            </div>
+            <div class="col-lg-6 mb-3">
+                <h5 class="text-white">Location</h5>
+                <img src="../../../public/dist/img/Map.png" alt="" height="75%" width="100%">
+                <div class="col mt-4">
+                    <i class="bi bi-telephone text-white"></i>
+                    <span class="text-white ms-2">02123371523</span> 
+                    <i class="bi bi-envelope text-white ms-2"></i>
+                    <span class="text-white ms-2">SweetHome@gmail.com</span> 
+                </div>
+                <div class="col mt-4 ">
+                        <a href="#"><i class="bi bi-instagram text-white"></i></a>
+                        <a href="#"><i class="bi bi-facebook text-white"></i></a>
+                        <a href="#"><i class="bi bi-telegram text-white"></i></a>
+                        <a href="#"><i class="bi bi-twitter text-white "></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
